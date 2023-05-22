ebd5cfbc7036d7f1a5cbb829b34873bece2d1be1
Danmusik22 <Danmusik22@hotmail.com>
2023-05-22 13:29:18 +0200

    Initial Commit

---
+++ Quizzer/QuizzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzerApp.swift
+//  Quizzer
+//
+//  Created by Dan Søndergaard on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
